43e8058d4a11d11d70d00ad956c803f9a7d91383
sanjeevkumar <sanjeev.kumar@rapidfunnel.com>
2025-02-08 11:59:47 +0530

    Allow new line as delimiter

--- src/addNumber.ts
+++ src/addNumber.ts
@@ -2,7 +2,7 @@ export function addNumber(numbers: string): number {
   if (!numbers) return 0;
 
   return numbers
-    .split(",")
+    .split(/[\n,]/) // Split by both newline (\n) and comma (,)
     .map((num) => parseInt(num, 10) || 0) // Convert to numbers, default to 0 if invalid
     .reduce((sum, num) => sum + num, 0); // Sum up numbers
 }
--- test/addNumber.test.ts
+++ test/addNumber.test.ts
@@ -20,4 +20,9 @@ describe("String Calculator - add()", () => {
     expect(addNumber("10,20,30")).toBe(60);
   });
 
+   test("supports new lines as delimiters", () => {
+     expect(addNumber("1\n2,3")).toBe(6);
+     expect(addNumber("10\n20\n30")).toBe(60);
+   });
+
 });
